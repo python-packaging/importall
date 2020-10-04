55f353e8b997045dd87e43afa9d92849e6e57db2
Mark Rofail <mark.rofail@shahry.app>
2020-10-04 09:31:57 +0200

    [feat] parse multiple packages

--- importall/main.py
+++ importall/main.py
@@ -12,19 +12,22 @@ def main(argv: Optional[List[str]] = None) -> None:
         "--exclude", default="tests", help="comma-separated directories to ignore"
     )
     parser.add_argument("--root", help="Search under this dir")
-    parser.add_argument("package", metavar="PACKAGE", help="Name of package to test")
+    parser.add_argument("package", nargs='+', metavar="PACKAGE", help="Name of package(s) to test")
     args = parser.parse_args(argv)
 
-    assert "." not in args.package, "give top level package name"
     if args.root:
         sys.path.insert(0, args.root)
 
-    filename = __import__(args.package).__file__
-    assert filename is not None, "namespace packages not supported"
-    assert filename.endswith("__init__.py"), "single modules not supported"
-
     exclude_set = set(args.exclude.split(","))
-    base_path = Path(filename).parent.parent
-    for name in sorted(find_importable_names(base_path, args.package, exclude_set)):
-        __import__(name)
-        print(f"{name} ok")
+
+    for package in args.package:
+        assert "." not in package, f"{package}: give top level package name"
+
+        filename = __import__(package).__file__
+        assert filename is not None, f"{package}: namespace packages not supported"
+        assert filename.endswith("__init__.py"), f"{package}: single modules not supported"
+
+        base_path = Path(filename).parents[1]
+        for name in sorted(find_importable_names(base_path, package, exclude_set)):
+            __import__(name)
+            print(f"{name} ok")
